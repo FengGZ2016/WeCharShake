7d31dd3311590a65f003ec53c09872e9d8153689
冯国富 <fgz2016@outlook.com>
2017-06-14 00:31:56 +0800

    封装添加mp3文件方法

--- app/src/main/java/com/example/wecharshake/Util.java
+++ app/src/main/java/com/example/wecharshake/Util.java
@@ -1,9 +1,14 @@
 package com.example.wecharshake;
 
+import android.content.Context;
+import android.media.SoundPool;
 import android.view.animation.Animation;
 import android.view.animation.AnimationSet;
 import android.view.animation.TranslateAnimation;
 import android.widget.RelativeLayout;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 作者：国富小哥
@@ -37,20 +42,38 @@ public class Util {
 
         //下面的图片的动画
         AnimationSet animationSetDn=new AnimationSet(true);
-        TranslateAnimation start2 = new TranslateAnimation(
+        TranslateAnimation end0 = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0f,
                 Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF,
                 +0.5f);
-        start2.setDuration(1000);
-        TranslateAnimation start3 = new TranslateAnimation(
+        end0.setDuration(1000);
+        TranslateAnimation end1 = new TranslateAnimation(
                 Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF, 0f,
                 Animation.RELATIVE_TO_SELF, 0f, Animation.RELATIVE_TO_SELF,
                 -0.5f);
-        start3.setDuration(1000);
-        start3.setStartOffset(1000);
-        animationSetDn.addAnimation(start2);
-        animationSetDn.addAnimation(start3);
+        end1.setDuration(1000);
+        end1.setStartOffset(1000);
+        animationSetDn.addAnimation(end0);
+        animationSetDn.addAnimation(end1);
         mImgDn.startAnimation(animationSetDn);
+    }
+
+
+    /**
+     * 添加声音资源文件
+     * */
+    public static Map<Integer,Integer> loadSound(final SoundPool pool, final Context context){
+        final Map<Integer,Integer> soundPoolMap=new HashMap<>();
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        soundPoolMap.put(0,pool.load(context,R.raw.shake_sound_male,1));
+                        soundPoolMap.put(1,pool.load(context,R.raw.shake_match,1));
+                    }
+                }
+        ).start();
+        return soundPoolMap;
     }
 
 }
