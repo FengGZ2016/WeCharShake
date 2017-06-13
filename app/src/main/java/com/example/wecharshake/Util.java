4d7416427fa4e19020ed085e5774729172c26475
冯国富 <fgz2016@outlook.com>
2017-06-13 23:31:26 +0800

    改善布局界面

---
+++ app/src/main/java/com/example/wecharshake/Util.java
@@ -0,0 +1,21 @@
+package com.example.wecharshake;
+
+import android.view.animation.AnimationSet;
+import android.widget.LinearLayout;
+
+/**
+ * 作者：国富小哥
+ * 日期：2017/6/12
+ * Created by Administrator
+ */
+
+public class Util {
+    /**
+     * 开启动画
+     * */
+    public static void startAnim(LinearLayout mImgTop,LinearLayout mImgBom){
+        AnimationSet animationSetUp=new AnimationSet(true);
+       // TranslateAnimation start0=new TranslateAnimation(Animation.RELATIVE_TO_SELF,of)
+    }
+
+}
